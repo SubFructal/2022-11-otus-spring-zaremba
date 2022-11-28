52b992b8e87e0476f8c67c6294a5d61361678e73
sanny <sannyfanny.y@list.ru>
2022-11-28 08:18:41 +0300

    some updates ConsoleIOServiceTest

--- hw001/src/test/java/ru/otus/homework/services/ConsoleIOServiceTest.java
+++ hw001/src/test/java/ru/otus/homework/services/ConsoleIOServiceTest.java
@@ -14,14 +14,11 @@ class ConsoleIOServiceTest {
     private static final String LINE_SEPARATOR = System.lineSeparator();
     private static final String TEXT_FOR_PRINT = "Test text for print";
 
-    private ByteArrayOutputStream byteArrayOutputStream;
-    private IOService ioService;
-
     @DisplayName("должен корректно выводить данные в консоль")
     @Test
     void shouldPrintTestTextCorrectly() {
-        byteArrayOutputStream = new ByteArrayOutputStream();
-        ioService = new ConsoleIOService(new PrintStream(byteArrayOutputStream));
+        var byteArrayOutputStream = new ByteArrayOutputStream();
+        var ioService = new ConsoleIOService(new PrintStream(byteArrayOutputStream));
         ioService.output(TEXT_FOR_PRINT);
 
         assertThat(byteArrayOutputStream.toString()).isEqualTo(TEXT_FOR_PRINT + LINE_SEPARATOR);
