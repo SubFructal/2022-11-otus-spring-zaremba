3d76638379cf5c500fdd036cab4eb23a7847b4bb
Alex Zaremba <sashadjram@gmail.com>
2023-06-06 22:01:54 +0300

    hw016: update application.yml, main

--- hw016/src/main/java/ru/otus/homework/Application.java
+++ hw016/src/main/java/ru/otus/homework/Application.java
@@ -8,11 +8,11 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
 
-        System.out.printf("%nГлавная: %n%s%n", "http://localhost:8080");
+        System.out.printf("%nГлавная: %n%s%n", "http://localhost:8089");
         System.out.printf("логин: %s; пароль: %s%n", "admin", "admin123");
         System.out.printf("логин: %s; пароль: %s%n", "user", "user456");
 
-        System.out.printf("%nActuator: %n%s%n%n", "http://localhost:8080/actuator");
+        System.out.printf("%nActuator: %n%s%n%n", "http://localhost:8089/actuator");
 
     }
 }
